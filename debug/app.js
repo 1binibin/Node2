e410fc4444ca50b7a3e69c9ac70a39eb8310e212
1binibin <namwonbin@gmail.com>
2021-10-13 22:46:25 +0900

    ...

---
+++ debug/app.js
@@ -0,0 +1,24 @@
+function sayHello() {
+  console.log('hello');
+  console.log(1111);
+  console.log(3333);
+}
+
+function calculate(x, y) {
+  console.log('calculating...');
+  const result = x + y;
+  console.log('result: ', result);
+  sayHello();
+  return result;
+}
+
+calculate(1, 2)
+
+const stop = 4;
+console.log('...., loopings, .....');
+for (let i = 0; i < 10; i++) {
+  console.log('count', i);
+  if (i === stop) {
+    break;
+  }
+}
